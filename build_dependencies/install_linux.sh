33dd98ce46e18e260e1cbf3f920f17ed1f31f55f
Cedric Leblond-Menard <cedlmen@hotmail.com>
2022-06-14 16:28:14 -0400

    WIP

--- build_dependencies/install_linux.sh
+++ build_dependencies/install_linux.sh
@@ -1,6 +1,8 @@
+set -xe
+
 mkdir -p deps
 cd deps
-get eigen
+# get eigen
 curl -OL https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
 tar -zxf eigen-3.3.9.tar.gz
 
@@ -8,10 +10,11 @@ rm -rf libccd
 git clone --depth 1 --branch v2.1 https://github.com/danfis/libccd.git
 
 rm -rf octomap 
-git clone --depth 1 --branch v1.8.0 https://github.com/OctoMap/octomap.git
+git clone --depth 1 --branch v1.9.7 https://github.com/OctoMap/octomap.git
 
 rm -rf fcl
-git clone --depth 1 --branch v0.6.1 https://github.com/flexible-collision-library/fcl.git
+git clone --depth 1 --branch 0.7.0 https://github.com/flexible-collision-library/fcl.git
+
 
 echo "Install eigen"
 cmake -B build -S eigen-3.3.9
